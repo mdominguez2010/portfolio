91a53ebc06a49ac6e9727fac7cbf580c9a0936b8
Marcos Dominguez <md.ghsd@gmail.com>
2022-03-21 10:54:56 -0700

    update active buttons

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ function PageTransitions(){
             this.className += ' active-btn';
         })
     }
+
+    //Sections activae class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //resmove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
